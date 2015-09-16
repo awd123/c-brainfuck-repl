f347434214aa6b38b73d5f5934ebf121bf845d04
Austin Dunn <awd123ps@gmail.com>
2015-09-16 17:20:20 -0500

    Updated to 0.0.3.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
 
 int main(void) {
+	const char *DELIMITER = NULL;
 	char mem_array[] = {0};
-	char *pointer = mem_array;
+	char *p = mem_array;
+	char line[1000];
 
-	puts("Welcome to the C Brainf*** REPL version 0.0.1");
+	puts("Welcome to the C Brainf*** REPL version 0.0.3");
 
 	void repl(void) {
-		while (true) {
-			char line[];
-			printf("Enter a command: ");
+		int i;
 
+		while (1) {
+			printf("%c", 62);
+			fgets(line, 1000, stdin);
+
+			for (i = 0; i < sizeof(line); i++) {
+				char t = line[i];
+
+				if (t == '>') {
+					p++;
+				} else if (t == '<') {
+					p--;
+				} else if (t == '+') {
+					*p++;
+				} else if (t == '-') {
+					*p--;
+				} else if (t == '.') {
+					printf("\n%c", *p);
+				} else if (t == ',') {
+					scanf("%c", &*p);
+				} else if (line[0..3] == "quit") {
+					break;
+				}
+			};
+		}
+	}
+
+	repl();
+
+	return 0;
+}
